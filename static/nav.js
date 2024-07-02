9b9d64cac0676580f399ec1f39a0bbebbd3f67ca
Dibakar Sigdel <sigdeldkr@gmail.com>
2024-07-02 00:32:21 -0700

    upd

---
+++ static/nav.js
@@ -0,0 +1,13 @@
+
+document.getElementById('theory').onclick = function() {
+    window.location.href = 'theory.html';
+};
+document.getElementById('projects').onclick = function() {
+    window.location.href = 'projects.html';
+};
+document.getElementById('questions').onclick = function() {
+    window.location.href = 'questions.html';
+};
+document.getElementById('cheatsheets').onclick = function() {
+    window.location.href = 'cheatsheets.html';
+};
